a0977afe9ff597401571b8065d55eb2ea88fa23b
unknown <3j36matsuo@gmail.com>
2020-01-28 15:01:26 +0900

    修正

--- raspi.py
+++ raspi.py
@@ -45,8 +45,8 @@ on_ref = db.collection('led').where(u'led', u'==', u'ON')
 off_ref = db.collection('led').where(u'led', u'==', u'OFF')
 
 # 監視を開始する
-#doc_watch = on_ref.on_snapshot(on_snapshot)
-#doc_watch = off_ref.on_snapshot(on_snapshot)
+doc_watch = on_ref.on_snapshot(on_snapshot)
+doc_watch = off_ref.on_snapshot(on_snapshot)
 
 # 温度湿度気圧センサから値を取得してFirestoreに送信する部分
 # 「'''」で囲まれた部分はコメントアウトされているので，参考にすること．
